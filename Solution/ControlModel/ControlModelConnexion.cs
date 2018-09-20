39db071331434641aadf55984ae3c6ac0177b15b
TartPanther47 <cfmgp@hotmail.com>
2018-09-20 15:40:14 -0400

    Mdp login fonctionnel

--- Solution/ControlModel/ControlModelConnexion.cs
+++ Solution/ControlModel/ControlModelConnexion.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 using System.Windows.Input;
 using VitAdmin.MVVM;
 
@@ -11,15 +12,14 @@ namespace VitAdmin.ControlModel
     public class ControlModelConnexion : ObjetObservable
     {
         public string Usager { get; set; }
-        public string MotDePasse { get; set; }
 
         public ICommand CmdConnexion
         {
             get
             {
-                return new CommandeDeleguee(() =>
+                return new CommandeDeleguee(password =>
                 {
-                    System.Windows.MessageBox.Show(Usager);
+                    System.Windows.MessageBox.Show(Usager + ", " + (password as PasswordBox).Password);
                 });
             }
         }
--- Solution/MVVM/CommandeDeleguee.cs
+++ Solution/MVVM/CommandeDeleguee.cs
@@ -9,14 +9,14 @@ namespace VitAdmin.MVVM
 {
     public class CommandeDeleguee : ICommand
     {
-        private readonly Action action;
+        private readonly Action<object> action;
         public event EventHandler CanExecuteChanged;
 
         public bool CanExecute(object parameter) => true;
 
-        public void Execute(object parameter) => action();
+        public void Execute(object parameter) => action(parameter);
 
-        public CommandeDeleguee(Action action)
+        public CommandeDeleguee(Action<object> action)
             => this.action = action;
     }
 }
