032dee49e32f1612482ed7689b6e620fd6a0879b
Maxduf007 <maxime_duf@hotmail.com>
2018-09-26 16:26:51 -0400

    PageCreerHospitalisation

---
+++ Solution/Control/placeHolder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VitAdmin.Control
+{
+    public class placeHolder
+    {
+        public string texte { get; set; }
+
+        public placeHolder(string texte, EventArgs e)
+        {
+
+        }
+    }
+}
