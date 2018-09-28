c2435b0efde1c69ad0bd3d4cc02ad62be3814dbf
Clément Gassmann-Prince <1661861@cstj.qc.ca>
2018-09-28 17:16:28 -0400

    Écran modifier un traitement: accès BD

--- Solution/ControlModel/ControlModelListeTraitementsAvecEtapes.cs
+++ Solution/ControlModel/ControlModelListeTraitementsAvecEtapes.cs
@@ -3,18 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VitAdmin.Model;
 using VitAdmin.MVVM;
 
 namespace VitAdmin.ControlModel
 {
     public class ControlModelListeTraitementsAvecEtapes : ObjetObservable
     {
+        public List<Traitement> Traitements { get; set; }
 
-
-
-        public ControlModelListeTraitementsAvecEtapes()
-        {
-
-        }
+        public ControlModelListeTraitementsAvecEtapes(List<Traitement> traitements)
+            => Traitements = traitements;
     }
 }
