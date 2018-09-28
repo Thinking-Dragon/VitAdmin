5029f2c80437b21bf1674b2cd76ee6d2715a1116
Clément Gassmann-Prince <1661861@cstj.qc.ca>
2018-09-28 17:48:14 -0400

    Ajout datamodel étape et traitement

---
+++ Solution/Data/DataModelEtape.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VitAdmin.Model;
+
+namespace VitAdmin.Data
+{
+    public static class DataModelEtape
+    {
+        public static List<Etape> GetEtapes(int idTraitement)
+        {
+            List<Etape> etapes = new List<Etape>();
+
+            if(ConnexionBD.Instance().EstConnecte())
+            {
+                ConnexionBD.Instance().ExecuterRequete(
+                    String.Format(
+                        "SELECT description FROM Etapes WHERE idTraitement = {0}", idTraitement
+                    ), lecteur =>
+                    {
+                        etapes.Add(new Etape
+                        {
+                            Description = 
+                        })
+                    }
+                );
+            }
+
+            return etapes;
+        }
+    }
+}
