6bacbc30ffa0491a469e761f5c66525b51b3c28b
Félix MOREAU <1282123@cstj.qc.ca>
2018-10-19 17:31:52 -0400

    Ajout fenêtre pour profil de professionnel usercontrol et view

---
+++ Solution/ControlModel/ControlModelProfessionnelProfil.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VitAdmin.Model;
+
+namespace VitAdmin.ControlModel
+{
+    class ControlModelProfessionnelProfil
+    {
+        Employe Employe { get; set; }
+    }
+}
---
+++ Solution/ViewModel/ViewModelProfessionnelProfil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using VitAdmin.Model;
+using VitAdmin.MVVM;
+
+namespace VitAdmin.ViewModel
+{
+    class ViewModelProfessionnelProfil
+    {
+        Employe Employe { get; set; }
+        public ICommand CmdBtnCreer
+        {
+            get
+            {
+                return new CommandeDeleguee(newEmploye =>
+                {
+
+                });
+            }
+        }
+    }
+}
